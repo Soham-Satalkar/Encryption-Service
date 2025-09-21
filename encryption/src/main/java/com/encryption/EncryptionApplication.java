52e26e4e44ba634a9d9397eb0a7e258aaf62f926
Soham-Satalkar <sohamsatalkar18@gmail.com>
2025-09-21 14:57:46 +0530

    Update : RSA Key Pair Generation Service Implemented.

--- encryption/src/main/java/com/encryption/EncryptionApplication.java
+++ encryption/src/main/java/com/encryption/EncryptionApplication.java
@@ -1,13 +1,25 @@
 package com.encryption;
 
+import com.encryption.util.KeyPairRSAGenerator;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 public class EncryptionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncryptionApplication.class, args);
+
 	}
+
+//    @PostConstruct
+//    public void init() throws NoSuchAlgorithmException, IOException {
+//        KeyPairRSAGenerator generator = new KeyPairRSAGenerator();
+//        generator.generateRSAKey();  // Called after bean creation
+//    }
 
 }
--- encryption/src/main/java/com/encryption/util/KeyPairRSAGenerator.java
+++ encryption/src/main/java/com/encryption/util/KeyPairRSAGenerator.java
@@ -1,11 +1,25 @@
 package com.encryption.util;
 
 
-import java.security.*;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.util.Base64;
+
+@Slf4j
+@Component
 public class KeyPairRSAGenerator {
 
-    public void generateRSAKey() throws NoSuchAlgorithmException {
+    private static final Logger log = LoggerFactory.getLogger(KeyPairRSAGenerator.class);
+
+    public void generateRSAKey() throws NoSuchAlgorithmException, IOException {
+
+        log.info("----------RSA Key Generation Started----------");
 
         //1.Generate RSA Key Pair
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
@@ -17,6 +31,24 @@ public class KeyPairRSAGenerator {
         PublicKey publicKey = keyPair.getPublic();
         PrivateKey privateKey = keyPair.getPrivate();
 
-    }
+        //3.Convert key to Base64-encoded Strings
+        String publicKeyPEM = "BEGIN PUBLIC KEY--\n"
+                + Base64.getMimeEncoder(64,new byte[]{'\n'}).encodeToString(publicKey.getEncoded())
+                + "\n--END PUBLIC KEY--\n" ;
 
+        String privateKeyPEM = "BEGIN PRIVATE KEY--\n"
+                + Base64.getMimeEncoder(64,new byte[]{'\n'}).encodeToString(privateKey.getEncoded())
+                + "\n--END PRIVATE KEY--\n";
+
+        //4.Save to files
+        try(FileOutputStream pubOut = new FileOutputStream("public_key.pem")){
+            pubOut.write(publicKeyPEM.getBytes());
+        }
+        try(FileOutputStream privOut = new FileOutputStream("private_key.pem")){
+            privOut.write(privateKeyPEM.getBytes());
+        }
+
+        log.info("----------RSA Key Generation End----------");
+
+    }
 }
